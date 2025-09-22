1e10851fdbe7fe3448fe6a2ae7257a1cf6bf2c2b
skhasan536 <hasan.shaikh@wybrid.com>
2025-09-22 21:25:06 +0530

    Added fields to search

--- src/controllers/search-controllers.js
+++ src/controllers/search-controllers.js
@@ -1,15 +1,14 @@
-import {searchSchoolsService} from '../services/search-services.js';
+import { searchSchoolsService } from '../services/search-services.js';
 import { toSchoolCardModels } from '../utils/utils.js';
 
 export const searchSchool = async (req, res) => {
 
   try {
-    let { search, boards, cities, state, page = 1, limit = 10 } = req.query;
+    let { search, boards, cities, state, schoolMode, genderType, feeRange, page = 1, limit = 10 } = req.query;
 
     page = parseInt(page);
     limit = parseInt(limit);
-
-    const result = await searchSchoolsService({ search, boards, cities, state, page, limit });
+    const result = await searchSchoolsService({ search, boards, cities, state, schoolMode, genderType, feeRange, page, limit });
 
     if (!result.data.length) {
       return res.status(404).json({ status: "failed", message: "No schools found for the given search." });
@@ -27,5 +26,5 @@ export const searchSchool = async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ status: "failed", message: error.message || "Internal Server Error" });
   }
-  
+
 };
