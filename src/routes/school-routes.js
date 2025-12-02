fcc6e63d9e758e12077fdea109c4e318c52e0517
Parth-Ranjan <161974405+Parth-Ranjan@users.noreply.github.com>
2025-12-02 18:27:41 +0530

    Update POST route for adding school with authId

--- src/routes/school-routes.js
+++ src/routes/school-routes.js
@@ -82,7 +82,7 @@ router.get("/count", getStudentsCount);
 router.get('/schools/:id', getSchoolById);
 router.put('/schools/:id', updateSchoolInfo);
 router.delete('/schools/:id', deleteSchool);
-router.post('/schools/:authId', addSchoolByAuth);    // create and attach authId
+router.post('/schools/auth/:authId', addSchoolByAuth);    // create and attach authId
 router.get('/schools/auth/:authId', getSchoolByAuthId);
 
 
