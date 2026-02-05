9423b8f6448b970b7695822b8062a4510c61cdfd
skhasan536 <hasan.shaikh@wybrid.com>
2026-02-05 21:50:12 +0530

    Changed origin

--- src/app.js
+++ src/app.js
@@ -25,11 +25,7 @@ const app = express();
 // ✅ Middleware to handle form-data correctly
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'https://synzy.in',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-}));
+app.use(cors());
 
 
 //Routes for the API calls
